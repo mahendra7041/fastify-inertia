143c101000b7d885d55032a6e0b8491af0bc823c
mahendra7041 <chavdamahendra247@gmail.com>
2025-09-02 23:26:48 +0530

    fix: plugin config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fp from "fastify-plugin";
 import type { FastifyInstance } from "fastify";
-import { defineConfig, Inertia, Flash } from "node-inertiajs";
+import { defineConfig, Inertia, Flash, InertiaConfig } from "node-inertiajs";
 import { ViteDevServer, createServer as createViteServer } from "vite";
 
 declare module "fastify" {
@@ -13,35 +13,25 @@ declare module "fastify" {
   }
 }
 
-interface InertiaPluginOptions {
-  inertiaConfig: Parameters<typeof defineConfig>[0];
-  vite?: ViteDevServer | null;
-}
+// interface InertiaPluginOptions
 
-export default fp<InertiaPluginOptions>(async function inertiaPlugin(
+export default fp<InertiaConfig>(async function inertiaPlugin(
   fastify: FastifyInstance,
-  opts: InertiaPluginOptions
+  opts: InertiaConfig
 ) {
-  if (!opts.inertiaConfig) {
+  if (!opts) {
     throw new Error("Inertia.js configuration is required");
   }
-  const config = defineConfig(opts.inertiaConfig);
+  const config = defineConfig(opts);
 
   let vite: ViteDevServer | undefined;
 
-  if (process.env.NODE_ENV !== "production") {
-    vite =
-      opts.vite ??
-      (await createViteServer({
-        server: { middlewareMode: true },
-        appType: "custom",
-      }));
+  if (!config.vite && process.env.NODE_ENV !== "production") {
+    vite = await createViteServer(config.vite);
 
-    if (vite && !opts.vite) {
-      fastify.addHook("onRequest", (request, reply, done) => {
-        vite!.middlewares(request.raw, reply.raw, done);
-      });
-    }
+    fastify.addHook("onRequest", (request, reply, done) => {
+      vite!.middlewares(request.raw, reply.raw, done);
+    });
   }
 
   fastify.addHook("onRequest", (request, reply, done) => {
